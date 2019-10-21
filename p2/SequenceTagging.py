fa13566374ee192b181280deb329d91a8366c4ca
abu qader <aspctub@gmail.com>
2019-10-20 20:19:12 -0400

    me and abu dicked around for 30 min

--- p2/SequenceTagging.py
+++ p2/SequenceTagging.py
@@ -2,8 +2,10 @@ import csv
 import numpy as np
 import random
 import ast
-
-import nltk, nltk.classify.util, nltk.metrics
+import math
+import nltk
+import nltk.classify.util
+import nltk.metrics
 from nltk.classify import MaxentClassifier
 from nltk.collocations import BigramCollocationFinder
 from nltk.metrics import BigramAssocMeasures
@@ -11,7 +13,11 @@ from nltk.probability import FreqDist, ConditionalFreqDist
 from sklearn.model_selection import cross_validate
 from nltk.classify import MaxentClassifier
 
+
 def calculate_transition_probs(labels):
+    """
+    dict of bigram sequences to their counts
+    """
     counts = {}
     for label_set in labels:
         for i in range(len(label_set)):
@@ -24,10 +30,15 @@ def calculate_transition_probs(labels):
             curr_label = str(label_set[i])
 
             counts[curr_label] = counts.get(curr_label, 0) + 1
-            counts[prev_label + curr_label] = counts.get(prev_label + curr_label, 0) + 1
+            counts[prev_label +
+                   curr_label] = counts.get(prev_label + curr_label, 0) + 1
     return counts
 
+
 def hmm_counts(words, labels):
+    """
+
+    """
     counts = {"<UNK>": 0}
     x = 0
     for x in range(len(labels)):
@@ -35,9 +46,11 @@ def hmm_counts(words, labels):
             curr_word = words[x][i]
             curr_label = str(labels[x][i])
             if(not curr_word+curr_label in counts.keys()):
-              counts["<UNK>"] +=1
-            counts[curr_word + curr_label] = counts.get(curr_word + curr_label, 0) + 1
+                counts["<UNK>"] += 1
+            counts[curr_word +
+                   curr_label] = counts.get(curr_word + curr_label, 0) + 1
     return counts
+
 
 def return_featureset(words, pos_seq, i):
     feature_dict = {}
@@ -49,7 +62,7 @@ def return_featureset(words, pos_seq, i):
     else:
         feature_dict["2before"] = None
         feature_dict["1before"] = None
-    if i<len(words)-2:
+    if i < len(words)-2:
         feature_dict["1after"] = words[i+1]
         feature_dict["2after"] = words[i+2]
     else:
@@ -57,7 +70,6 @@ def return_featureset(words, pos_seq, i):
         feature_dict["2after"] = None
 
     return feature_dict
-
 
 
 def create_classifier():
@@ -73,8 +85,9 @@ def create_classifier():
                 feature_dict = return_featureset(words, pos_seq, i)
                 feature_set.append((feature_dict, label_seq[i]))
 
-    classifier = nltk.MaxentClassifier.train(feature_set,max_iter=2)
+    classifier = nltk.MaxentClassifier.train(feature_set, max_iter=2)
     return classifier
+
 
 def viterbi_segment(text, pos_seq, counts, classifier, hmmcounts, isHMM):
     n = len(text)
@@ -87,12 +100,28 @@ def viterbi_segment(text, pos_seq, counts, classifier, hmmcounts, isHMM):
     for i in range(2):
         BPTR[i] = [0] * n
 
+    word0 = "<UNK>"
+    word1 = "<UNK>"
+    if(text[0]+"0" in hmmcounts.keys()):
+        word0 = text[0]+"0"
+    elif(text[0]+"1" in hmmcounts.keys()):
+        word1 = text[0]+"1"
+
     feature_dict = return_featureset(words, pos_seq, 0)
+    if(isHMM):
+        SCORE[0][0] = (counts["<s>0"]/counts["<s>"]) * \
+            (hmmcounts[word0]/counts["0"])
+        SCORE[1][0] = (counts["<s>0"]/counts["<s>"]) * \
+            (hmmcounts[word1]/counts["1"])
+    else:
+        SCORE[0][0] = (counts["<s>0"]/counts["<s>"]) * \
+            classifier.prob_classify(featureset=feature_dict).prob(0)
+        SCORE[1][0] = (counts["<s>1"]/counts["<s>"]) * \
+            classifier.prob_classify(featureset=feature_dict).prob(1)
+
     probs = classifier.prob_classify(featureset=feature_dict)
-    SCORE[0][0] = (counts["<s>0"]/counts["<s>"]) * classifier.prob_classify(featureset=feature_dict).prob(0)
-    SCORE[1][0] = (counts["<s>1"]/counts["<s>"]) * classifier.prob_classify(featureset=feature_dict).prob(1)
-    #SCORE[0][0] =  classifier.prob_classify(featureset=feature_dict).prob(0)
-    #SCORE[1][0] =  classifier.prob_classify(featureset=feature_dict).prob(1)
+    # SCORE[0][0] =  classifier.prob_classify(featureset=feature_dict).prob(0)
+    # SCORE[1][0] =  classifier.prob_classify(featureset=feature_dict).prob(1)
     BPTR[0][0] = None
     BPTR[1][0] = None
 
@@ -100,52 +129,59 @@ def viterbi_segment(text, pos_seq, counts, classifier, hmmcounts, isHMM):
 
     for t in range(1, n):
         if(not isHMM):
-          if classifier.prob_classify(feature_dict).prob(0) < classifier.prob_classify(feature_dict).prob(1)+.5:
-              #print("HERE")
-              #print(classifier.prob_classify(feature_dict).prob(0))
-              #print(classifier.prob_classify(feature_dict).prob(1))
-              c+=1
-          feature_dict = return_featureset(words, pos_seq, t)
+            if classifier.prob_classify(feature_dict).prob(0) < classifier.prob_classify(feature_dict).prob(1)+.5:
+                # print("HERE")
+                # print(classifier.prob_classify(feature_dict).prob(0))
+                # print(classifier.prob_classify(feature_dict).prob(1))
+                c += 1
+            feature_dict = return_featureset(words, pos_seq, t)
 
-          if SCORE[0][t-1]*(counts["00"]/counts["0"]) >= SCORE[1][t-1] * (counts["10"]/counts["1"]):
-              #SCORE[0][t] = SCORE[0][t-1]* (counts["00"]/counts["0"]) * classifier.prob_classify(feature_dict).prob(0)
-              SCORE[0][t] = SCORE[0][t-1] * classifier.prob_classify(feature_dict).prob(0)
-              BPTR[0][t] = 0
-          else:
-              #SCORE[0][t] = SCORE[1][t-1]* (counts["10"]/counts["1"]) * classifier.prob_classify(feature_dict).prob(0)
-              SCORE[0][t] = SCORE[1][t-1] * classifier.prob_classify(feature_dict).prob(0)
-              BPTR[0][t] = 1
+            if SCORE[0][t-1]*(counts["00"]/counts["0"]) >= SCORE[1][t-1] * (counts["10"]/counts["1"]):
+                # SCORE[0][t] = SCORE[0][t-1]* (counts["00"]/counts["0"]) * classifier.prob_classify(feature_dict).prob(0)
+                SCORE[0][t] = SCORE[0][t-1] * \
+                    classifier.prob_classify(feature_dict).prob(0)
+                BPTR[0][t] = 0
+            else:
+                # SCORE[0][t] = SCORE[1][t-1]* (counts["10"]/counts["1"]) * classifier.prob_classify(feature_dict).prob(0)
+                SCORE[0][t] = SCORE[1][t-1] * \
+                    classifier.prob_classify(feature_dict).prob(0)
+                BPTR[0][t] = 1
 
-          if SCORE[0][t-1]*(counts["01"]/counts["0"]) >= SCORE[1][t-1] * (counts["11"]+1000/counts["1"]):
-              #SCORE[1][t] = SCORE[0][t-1]*(counts["01"]/counts["0"]) * classifier.prob_classify(feature_dict).prob(1)
-              SCORE[1][t] = SCORE[0][t-1] * (classifier.prob_classify(feature_dict).prob(1)+.5 )
-              BPTR[1][t] = 0
-          else:
-              #SCORE[1][t] = SCORE[1][t-1]*(counts["11"]/counts["1"]) * classifier.prob_classify(feature_dict).prob(1)
-              SCORE[1][t] = SCORE[1][t-1] * (classifier.prob_classify(feature_dict).prob(1)+.5 )
-              BPTR[1][t] = 1
+            if SCORE[0][t-1]*(counts["01"]/counts["0"]) >= SCORE[1][t-1] * (counts["11"]+1000/counts["1"]):
+                # SCORE[1][t] = SCORE[0][t-1]*(counts["01"]/counts["0"]) * classifier.prob_classify(feature_dict).prob(1)
+                SCORE[1][t] = SCORE[0][t-1] * \
+                    (classifier.prob_classify(feature_dict).prob(1)+.5)
+                BPTR[1][t] = 0
+            else:
+                # SCORE[1][t] = SCORE[1][t-1]*(counts["11"]/counts["1"]) * classifier.prob_classify(feature_dict).prob(1)
+                SCORE[1][t] = SCORE[1][t-1] * \
+                    (classifier.prob_classify(feature_dict).prob(1)+.5)
+                BPTR[1][t] = 1
         else:
-          word0 = "<UNK>"
-          word1 = "<UNK>"
-          if(text[t]+"0" in hmmcounts.keys()):
-            word0 = text[t]+"0"
-          elif(text[t]+"1" in hmmcounts.keys()):
-            word1 = text[t]+"1"
+            word0 = "<UNK>"
+            word1 = "<UNK>"
+            if(text[t]+"0" in hmmcounts.keys()):
+                word0 = text[t]+"0"
+            elif(text[t]+"1" in hmmcounts.keys()):
+                word1 = text[t]+"1"
 
-          if SCORE[0][t-1]*(counts["00"]/counts["0"]) >= SCORE[1][t-1] * (counts["10"]/counts["1"]):
-              SCORE[0][t] = SCORE[0][t-1]* (counts["00"]/counts["0"]) * (hmmcounts[word0]/counts["0"])
-              BPTR[0][t] = 0
-          else:
-              SCORE[0][t] = SCORE[1][t-1]* (counts["10"]/counts["1"]) * (hmmcounts[word0]/counts["0"])
-              BPTR[0][t] = 1
+            if SCORE[0][t-1]*(counts["00"]/counts["0"]) >= SCORE[1][t-1] * (counts["10"]/counts["1"]):
+                SCORE[0][t] = math.exp(math.log(SCORE[0][t-1] +
+                                                (counts["00"]/counts["0"]) + (hmmcounts[word0]/counts["0"])))
+                BPTR[0][t] = 0
+            else:
+                SCORE[0][t] = math.exp(math.log(SCORE[1][t-1] *
+                                                (counts["10"]/counts["1"]) * (hmmcounts[word0]/counts["0"])))
+                BPTR[0][t] = 1
 
-          if SCORE[0][t-1]*(counts["01"]/counts["0"]) >= SCORE[1][t-1] * (counts["11"]/counts["1"]):
-              SCORE[1][t] = SCORE[0][t-1]*(counts["01"]/counts["0"]) * (hmmcounts[word1]/counts["1"])
-              BPTR[1][t] = 0
-          else:
-              SCORE[1][t] = SCORE[1][t-1]*(counts["11"]/counts["1"]) * (hmmcounts[word1]/counts["1"])
-              BPTR[1][t] = 1
-
+            if SCORE[0][t-1]*(counts["01"]/counts["0"]) >= SCORE[1][t-1] + (counts["11"]/counts["1"]):
+                SCORE[1][t] = math.exp(math.log(SCORE[0][t-1] +
+                                                (counts["01"]/counts["0"]) + (hmmcounts[word1]/counts["1"])))
+                BPTR[1][t] = 0
+            else:
+                SCORE[1][t] = math.exp(math.log(SCORE[1][t-1] *
+                                                (counts["11"]/counts["1"]) * (hmmcounts[word1]/counts["1"])))
+                BPTR[1][t] = 1
 
     sequence = []
     counter = n-1
@@ -158,11 +194,12 @@ def viterbi_segment(text, pos_seq, counts, classifier, hmmcounts, isHMM):
         a = BPTR[1][counter]
 
     while a is not None:
-        counter -=1
+        counter -= 1
         sequence.append(a)
         a = BPTR[a][counter]
-    #print(sequence[::-1])
+    # print(sequence[::-1])
     return sequence[::-1]
+
 
 def predict_classes():
     total_labels = []
@@ -186,7 +223,9 @@ def predict_classes():
         for line in lines:
             words = line[0].split()
             pos_seq = ast.literal_eval(line[1])
-            curr_sequence = viterbi_segment(words, pos_seq, counts, classifier, hmmC, True)
-            #print(curr_sequence)
+            curr_sequence = viterbi_segment(
+                words, pos_seq, counts, classifier, hmmC, True)
+            # print(curr_sequence)
+
 
 predict_classes()
