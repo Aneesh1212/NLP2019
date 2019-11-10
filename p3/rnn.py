4d227da701ffb5bc3897a5e2f5b165b9a5954572
abu qader <aspctub@gmail.com>
2019-11-10 14:56:09 -0500

    ading rnn self

--- p3/rnn.py
+++ p3/rnn.py
@@ -15,51 +15,56 @@ unk = '<UNK>'
 
 
 class RNN(nn.Module):
-	def __init__(self): # Add relevant parameters
-		super(RNN, self).__init__()
-		# Fill in relevant parameters
-		# Ensure parameters are initialized to small values, see PyTorch documentation for guidance
-		self.softmax = nn.LogSoftmax()
-		self.loss = nn.NLLLoss()
+    def __init__(self):  # Add relevant parameters
+        super(RNN, self).__init__()
+        self.W1 = nn.Linear(input_dim, h)
+        self.activation = nn.ReLU()
+        self.W2 = nn.Linear(h, h)
+        # Fill in relevant parameters
+        # Ensure parameters are initialized to small values, see PyTorch documentation for guidance
+        self.softmax = nn.LogSoftmax()
+        self.loss = nn.NLLLoss()
 
-	def compute_Loss(self, predicted_vector, gold_label):
-		return self.loss(predicted_vector, gold_label)	
+    def compute_Loss(self, predicted_vector, gold_label):
+        return self.loss(predicted_vector, gold_label)
 
-	def forward(self, inputs): 
-		#begin code
+    def forward(self, inputs):
+        # begin code
+        for val in inputs:
+            z1 = self.W1(inputs)
+            z2 = self.W2(z1)
 
-
-		predicted_vector = self.softmax() # Remember to include the predicted unnormalized scores which should be normalized into a (log) probability distribution
-		#end code
-		return predicted_vector
+        # Remember to include the predicted unnormalized scores which should be normalized into a (log) probability distribution
+        predicted_vector = self.softmax()
+        # end code
+        return predicted_vector
 
 # You may find the functions make_vocab() and make_indices from ffnn.py useful; you are free to copy them directly (or call those functions from this file)
 
 
+def main():  # Add relevant parameters
+    # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
+    train_data, valid_data = fetch_data()
 
-def main(): # Add relevant parameters
-	train_data, valid_data = fetch_data() # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
+    # Think about the type of function that an RNN describes. To apply it, you will need to convert the text data into vector representations.
+    # Further, think about where the vectors will come from. There are 3 reasonable choices:
+    # 1) Randomly assign the input to vectors and learn better embeddings during training; see the PyTorch documentation for guidance
+    # 2) Assign the input to vectors using pretrained word embeddings. We recommend any of {Word2Vec, GloVe, FastText}. Then, you do not train/update these embeddings.
+    # 3) You do the same as 2) but you train (this is called fine-tuning) the pretrained embeddings further.
+    # Option 3 will be the most time consuming, so we do not recommend starting with this
 
-	# Think about the type of function that an RNN describes. To apply it, you will need to convert the text data into vector representations.
-	# Further, think about where the vectors will come from. There are 3 reasonable choices:
-	# 1) Randomly assign the input to vectors and learn better embeddings during training; see the PyTorch documentation for guidance
-	# 2) Assign the input to vectors using pretrained word embeddings. We recommend any of {Word2Vec, GloVe, FastText}. Then, you do not train/update these embeddings.
-	# 3) You do the same as 2) but you train (this is called fine-tuning) the pretrained embeddings further. 
-	# Option 3 will be the most time consuming, so we do not recommend starting with this
+    model = RNN()  # Fill in parameters
+    optimizer = optim.SGD(model.parameters())
 
-	model = RNN() # Fill in parameters
-	optimizer = optim.SGD(model.parameters()) 
+    while not stopping_condition:  # How will you decide to stop training and why
+        optimizer.zero_grad()
+        # You will need further code to operationalize training, ffnn.py may be helpful
 
-	while not stopping_condition: # How will you decide to stop training and why
-		optimizer.zero_grad()
-		# You will need further code to operationalize training, ffnn.py may be helpful
+        predicted_vector = model(input_vector)
+        predicted_label = torch.argmax(predicted_vector)
+        # You may find it beneficial to keep track of training accuracy or training loss;
 
-		predicted_vector = model(input_vector)
-		predicted_label = torch.argmax(predicted_vector)
-		# You may find it beneficial to keep track of training accuracy or training loss; 
+        # Think about how to update the model and what this entails. Consider ffnn.py and the PyTorch documentation for guidance
 
-		# Think about how to update the model and what this entails. Consider ffnn.py and the PyTorch documentation for guidance
-
-		# You will need to validate your model. All results for Part 3 should be reported on the validation set. 
-		# Consider ffnn.py; making changes to validation if you find them necessary
-
+        # You will need to validate your model. All results for Part 3 should be reported on the validation set.
+        # Consider ffnn.py; making changes to validation if you find them necessary
