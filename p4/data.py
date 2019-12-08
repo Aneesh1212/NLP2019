7452deacdea1474d2a93a743be206877df9910df
abu qader <aspctub@gmail.com>
2019-12-08 13:47:41 -0500

    adding more features

--- p4/data.py
+++ p4/data.py
@@ -7,13 +7,13 @@ from numpy import loadtxt
 from keras.layers import Input, Concatenate, Conv2D, Flatten, Dense
 from keras.models import Model
 import pandas as pd
-
+from textblob import TextBlob
 nlp = en_core_web_sm.load()
 
 
 def model():
     # Define two input layers
-    input = Input((3, 96))
+    input = Input((6,))
     #vector_input = Input((6,))
 
     # Convolution + Flatten for the image
@@ -22,13 +22,14 @@ def model():
 
     # Concatenate the convolutional features and the vector input
     #concat_layer = Concatenate()([vector_input, flat_layer])
-    outputa = Dense(64)(input)
-    outputb = Dense(32)(outputa)
-    outputb = Flatten()(outputb)
-    outputc = Dense(2, activation='sigmoid')(outputb)
+    outputa = Dense(256)(input)
+    outputb = Dense(128)(outputa)
+    outputc = Dense(64)(outputb)
+    #outputc = Flatten()(outputc)
+    outputd = Dense(2, activation='sigmoid')(outputc)
 
     # define a model with a list of two inputs
-    model = Model(inputs=[input], outputs=outputc)
+    model = Model(inputs=[input], outputs=outputd)
     model.compile(loss='binary_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
     return(model)
@@ -47,11 +48,22 @@ def preprocess(data):
     for i, document in enumerate(data.iterrows()):
         doc_data = {"input_sentence": [], }
         doc = document[1]
-        doc_q = "{} {} {} {}".format(
-            doc["InputSentence1"], doc["InputSentence2"], doc["InputSentence3"], doc["InputSentence4"])
-        doc_data["input_sentence"] = nlp(doc_q).vector
+        # doc_q = "{} {} {} {}".format(
+        #    doc["InputSentence1"], doc["InputSentence2"], doc["InputSentence3"], doc["InputSentence4"])
+        doc_data["input_sentence"] = nlp(doc["InputSentence4"]).vector
+        blob_input = TextBlob(doc["InputSentence4"]
+                              ).sentences[::-1][0].sentiment.polarity
+        doc_data["input_sentiment"] = blob_input
+
+        # doc["RandomFifthSentenceQuiz1"]
         doc_data["output_one"] = nlp(doc["RandomFifthSentenceQuiz1"]).vector
+        blob_one = TextBlob(doc["RandomFifthSentenceQuiz1"]
+                            ).sentences[::-1][0].sentiment.polarity
+        doc_data["output_one_sentiment"] = blob_one
         doc_data["output_two"] = nlp(doc["RandomFifthSentenceQuiz2"]).vector
+        blob_two = TextBlob(doc["RandomFifthSentenceQuiz2"]
+                            ).sentences[::-1][0].sentiment.polarity
+        doc_data["output_two_sentiment"] = blob_two
         doc_data["label"] = doc["AnswerRightEnding"]
         dict_data.append(doc_data)
     return(dict_data)
@@ -63,8 +75,11 @@ def convert_to_input(dict_data):
     for v in dict_data:
         finaler = []
         finaler.append(v["input_sentence"])
+        finaler.append(np.array(v["input_sentiment"]))
         finaler.append(v["output_one"])
+        finaler.append(np.array(v["output_one_sentiment"]))
         finaler.append(v["output_two"])
+        finaler.append(np.array(v["output_two_sentiment"]))
         label = v["label"]
         if label == 1:
             Y.append([1, 0])
@@ -75,8 +90,11 @@ def convert_to_input(dict_data):
 
         finaler = []
         finaler.append(v["input_sentence"])
+        finaler.append(np.array(v["input_sentiment"]))
         finaler.append(v["output_two"])
+        finaler.append(np.array(v["output_two_sentiment"]))
         finaler.append(v["output_one"])
+        finaler.append(np.array(v["output_one_sentiment"]))
         if 3-label == 1:
             Y.append([1, 0])
         else:
