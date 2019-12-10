09b55ccbd8027160d1a929108cf0bfff55270b1e
abu qader <aspctub@gmail.com>
2019-12-09 23:42:56 -0500

    Adding code

--- p4/data.py
+++ p4/data.py
@@ -11,28 +11,23 @@ from textblob import TextBlob
 nlp = en_core_web_sm.load()
 
 
-def model():
-    # Define two input layers
-    input = Input((6,))
-    #vector_input = Input((6,))
+def ngram_generate(csv_path):
+    vals = []
+    csv = pd.read_csv(csv_path)
+    vals.append(csv["InputSentence1"].tolist())
+    vals.append(csv["InputSentence2"].tolist())
+    vals.append(csv["InputSentence3"].tolist())
+    vals.append(csv["InputSentence4"].tolist())
+    vals.append(csv["RandomFifthSentenceQuiz1"].tolist())
+    vals.append(csv["RandomFifthSentenceQuiz2"].tolist())
+    vectorizer = TfidfVectorizer(stop_words="english",
+                                 ngram_range=(1, 2))
+    vals = np.array(np.concatenate(vals).ravel().tolist())
+    training_features = vectorizer.fit_transform(vals)
+    return(vectorizer)
 
-    # Convolution + Flatten for the image
-    #conv_layer = Conv2D(32, (3, 3))(image_input)
-    #flat_layer = Flatten()(conv_layer)
 
-    # Concatenate the convolutional features and the vector input
-    #concat_layer = Concatenate()([vector_input, flat_layer])
-    outputa = Dense(256)(input)
-    outputb = Dense(128)(outputa)
-    outputc = Dense(64)(outputb)
-    #outputc = Flatten()(outputc)
-    outputd = Dense(2, activation='sigmoid')(outputc)
-
-    # define a model with a list of two inputs
-    model = Model(inputs=[input], outputs=outputd)
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam', metrics=['accuracy'])
-    return(model)
+l = ngram_generate('/content/train.csv')
 
 
 def read_csv(csv_path):
@@ -43,27 +38,32 @@ def generate_embeddings_per_document(document):
     return(nlp(document))
 
 
-def preprocess(data):
+def preprocess(data, l):
     dict_data = []
     for i, document in enumerate(data.iterrows()):
         doc_data = {"input_sentence": [], }
         doc = document[1]
-        # doc_q = "{} {} {} {}".format(
-        #    doc["InputSentence1"], doc["InputSentence2"], doc["InputSentence3"], doc["InputSentence4"])
-        doc_data["input_sentence"] = nlp(doc["InputSentence4"]).vector
+        doc_q = "{} {} {} {}".format(
+            doc["InputSentence1"], doc["InputSentence2"], doc["InputSentence3"], doc["InputSentence4"])
+        doc_data["input_sentence"] = nlp(doc_q).vector
         blob_input = TextBlob(doc["InputSentence4"]
                               ).sentences[::-1][0].sentiment.polarity
         doc_data["input_sentiment"] = blob_input
+        doc_data["input_bag"] = l.transform([doc_q]).toarray()
 
-        # doc["RandomFifthSentenceQuiz1"]
         doc_data["output_one"] = nlp(doc["RandomFifthSentenceQuiz1"]).vector
         blob_one = TextBlob(doc["RandomFifthSentenceQuiz1"]
                             ).sentences[::-1][0].sentiment.polarity
         doc_data["output_one_sentiment"] = blob_one
+        doc_data["output_one_bag"] = l.transform(
+            [doc["RandomFifthSentenceQuiz1"]]).toarray()
         doc_data["output_two"] = nlp(doc["RandomFifthSentenceQuiz2"]).vector
         blob_two = TextBlob(doc["RandomFifthSentenceQuiz2"]
                             ).sentences[::-1][0].sentiment.polarity
         doc_data["output_two_sentiment"] = blob_two
+        doc_data["output_two_bag"] = l.transform(
+            [doc["RandomFifthSentenceQuiz2"]]).toarray()
+
         doc_data["label"] = doc["AnswerRightEnding"]
         dict_data.append(doc_data)
     return(dict_data)
@@ -75,40 +75,134 @@ def convert_to_input(dict_data):
     for v in dict_data:
         finaler = []
         finaler.append(v["input_sentence"])
-        finaler.append(np.array(v["input_sentiment"]))
+        finaler.append(v["input_sentiment"])
+        finaler.append(v["input_bag"])
         finaler.append(v["output_one"])
-        finaler.append(np.array(v["output_one_sentiment"]))
+        finaler.append(v["output_one_sentiment"])
+        finaler.append(v["output_one_bag"])
         finaler.append(v["output_two"])
-        finaler.append(np.array(v["output_two_sentiment"]))
+        finaler.append(v["output_two_sentiment"])
+        finaler.append(v["output_two_bag"])
         label = v["label"]
         if label == 1:
             Y.append([1, 0])
         else:
             Y.append([0, 1])
         finaler = np.array(finaler)
+        new_f = []
+        for x in (finaler):
+            if type(x) == float:
+                zerod = np.zeros((30109, ))
+                zerod[0] = x
+                new_f.append(zerod)
+            else:
+                zerod = np.zeros((30109, ))
+                for i, av in enumerate(x.flatten()):
+                    zerod[i] = av
+                new_f.append(zerod)
+        finaler = np.array(new_f)
         X.append(finaler)
-
         finaler = []
         finaler.append(v["input_sentence"])
-        finaler.append(np.array(v["input_sentiment"]))
+        finaler.append(v["input_sentiment"])
+        finaler.append(v["input_bag"])
         finaler.append(v["output_two"])
-        finaler.append(np.array(v["output_two_sentiment"]))
+        finaler.append(v["output_two_sentiment"])
+        finaler.append(v["output_two_bag"])
         finaler.append(v["output_one"])
-        finaler.append(np.array(v["output_one_sentiment"]))
+        finaler.append(v["output_one_sentiment"])
+        finaler.append(v["output_one_bag"])
         if 3-label == 1:
             Y.append([1, 0])
         else:
             Y.append([0, 1])
         finaler = np.array(finaler)
+        new_f = []
+        for x in (finaler):
+            if type(x) == float:
+                zerod = np.zeros((30109, ))
+                zerod[0] = x
+                new_f.append(zerod)
+            else:
+                zerod = np.zeros((30109, ))
+                for i, av in enumerate(x.flatten()):
+                    zerod[i] = av
+                new_f.append(zerod)
+        finaler = np.array(new_f)
         X.append(finaler)
     return(np.array(X), np.array(Y))
 
 
 def train(csv_path):
     data = read_csv(csv_path)
-    # TODO this is redundant, just combine this line and next
-    dict_data = preprocess(data)
+    l = ngram_generate(csv_path)
+    dict_data = preprocess(data, l)
     X, Y = convert_to_input(dict_data)
-    model = model()
-    model.fit(X, Y, epochs=100)
+    model = model_generate()
+    model.fit(X, Y, epochs=10)
     return(model)
+
+# test set validation
+
+
+def preprocess_test(data):
+    dict_data = []
+    for i, document in enumerate(data.iterrows()):
+        doc_data = {"input_sentence": [], }
+        doc = document[1]
+        doc_data["id"] = doc["InputStoryid"]
+        doc_q = "{} {} {} {}".format(
+            doc["InputSentence1"], doc["InputSentence2"], doc["InputSentence3"], doc["InputSentence4"])
+        doc_data["input_sentence"] = nlp(doc_q).vector
+        blob_input = TextBlob(doc["InputSentence4"]
+                              ).sentences[::-1][0].sentiment.polarity
+        doc_data["input_sentiment"] = blob_input
+        doc_data["input_bag"] = l.transform([doc_q]).toarray()
+
+        # doc["RandomFifthSentenceQuiz1"]
+        doc_data["output_one"] = nlp(doc["RandomFifthSentenceQuiz1"]).vector
+        blob_one = TextBlob(doc["RandomFifthSentenceQuiz1"]
+                            ).sentences[::-1][0].sentiment.polarity
+        doc_data["output_one_sentiment"] = blob_one
+        doc_data["output_one_bag"] = l.transform(
+            [doc["RandomFifthSentenceQuiz1"]]).toarray()
+        doc_data["output_two"] = nlp(doc["RandomFifthSentenceQuiz2"]).vector
+        blob_two = TextBlob(doc["RandomFifthSentenceQuiz2"]
+                            ).sentences[::-1][0].sentiment.polarity
+        doc_data["output_two_sentiment"] = blob_two
+        doc_data["output_two_bag"] = l.transform(
+            [doc["RandomFifthSentenceQuiz2"]]).toarray()
+        dict_data.append(doc_data)
+    return(dict_data)
+
+
+def convert_to_input_test(dict_data):
+    X = []
+    Y = []
+    for v in dict_data:
+        finaler = []
+        finaler.append(v["input_sentence"])
+        finaler.append(v["input_sentiment"])
+        finaler.append(v["input_bag"])
+        finaler.append(v["output_one"])
+        finaler.append(v["output_one_sentiment"])
+        finaler.append(v["output_one_bag"])
+        finaler.append(v["output_two"])
+        finaler.append(v["output_two_sentiment"])
+        finaler.append(v["output_two_bag"])
+        Y.append(v["id"])
+        finaler = np.array(finaler)
+        new_f = []
+        for x in (finaler):
+            if type(x) == float:
+                zerod = np.zeros((30109, ))
+                zerod[0] = x
+                new_f.append(zerod)
+            else:
+                zerod = np.zeros((30109, ))
+                for i, av in enumerate(x.flatten()):
+                    zerod[i] = av
+                new_f.append(zerod)
+        finaler = np.array(new_f)
+        X.append(finaler)
+    return(np.array(X), np.array(Y))
---
+++ p4/model.py
@@ -0,0 +1,71 @@
+from data import read_csv, preprocess, convert_to_input, preprocess_test, convert_to_input_test
+# !python -m spacy download en_core_web_sm
+import spacy
+import en_core_web_sm
+import numpy as np
+import pandas as pd
+from numpy import loadtxt
+from keras.layers import Input, Concatenate, Conv2D, Flatten, Dense
+from keras.models import Model
+import pandas as pd
+from textblob import TextBlob
+import csv
+
+nlp = en_core_web_sm.load()
+
+
+def model_generate():
+    input = Input(shape=(9, 30109))
+    fdf = Flatten()(input)
+    outputa = Dense(512)(fdf)
+    outputb = Dense(128)(outputa)
+    outputc = Dense(64)(outputb)
+    outputd = Dense(2, activation='sigmoid')(outputc)
+    model = Model(inputs=[input], outputs=outputd)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+    return(model)
+
+
+def generate_csv(test_csv_path, X_test, Y_test):
+    res = []
+    for v in range(0, len(X_test)-10, 10):
+        res.append(modela.predict(X_test[v:v+10]))
+    clean = []
+    for g in res:
+        for l in g:
+            clean.append(l)
+    la = []
+    for i, l in enumerate(clean):
+        if l[0] > l[1]:
+            la.append((Y_test[i], 0))
+        else:
+            la.append((Y_test[i], 1))
+    with open(test_csv_path, 'w') as out:
+        csv_out = csv.writer(out)
+        csv_out.writerow(['id', 'prediction'])
+        for row in results:
+            csv_out.writerow(row)
+
+
+def validate_dataset(csv_path):
+    data_v = read_csv(csv_path)
+    dict_data = preprocess(data_v)
+    X_valid, Y_valid = convert_to_input(dict_data)
+    return(X_valid, Y_valid)
+
+
+def train(train_csv):
+    data = read_csv(train_csv)
+    dict_data = preprocess(data)
+    X, Y = convert_to_input(dict_data)
+    model = model_generate()
+    modela.fit(X, Y, epochs=10)
+    return(modela)
+
+
+def test(test_csv):
+    data = read_csv(test_csv)
+    dict_data = preprocess_test(test_csv)
+    X, Y = convert_to_input_test(test_csv)
+    return(X, Y)
